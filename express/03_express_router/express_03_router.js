409b2de2d64c56b4dc55d6d865b1406102b1482c
ex-Plant <konradantonik@gmail.com>
2025-05-23 16:33:25 +0200

    add express files

---
+++ express/03_express_router/express_03_router.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const testRouter = require("./test_router.js");
+const testRouter2 = require("./test_router_2.js");
+const testRouter3 = require("./test_router_3.js");
+
+app.get(`/`, (req, res) => {
+  res.status(200).send(`hello world again`);
+});
+
+app.use("/snake_helpers", testRouter);
+app.use("/test2", testRouter2);
+app.use("/test3", testRouter3);
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`listening: http:localhost:${port}`);
+});
